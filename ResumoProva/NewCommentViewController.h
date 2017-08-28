a48ee3fb989fcedebf1f9566e955afad014605d0
Felipe da Silva Barbosa <felipebarbosa@Felipes-Mac.local>
2017-08-27 17:18:15 -0700

    Upload picture via HTTP

--- ResumoProva/NewCommentViewController.h
+++ ResumoProva/NewCommentViewController.h
@@ -7,9 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface NewCommentViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+@interface NewCommentViewController : UIViewController
+        <UIImagePickerControllerDelegate,
+         UINavigationControllerDelegate,
+         CLLocationManagerDelegate,
+         MKMapViewDelegate,
+         UITextFieldDelegate>
+
+@property (strong, nonatomic) MKLocalSearch *localSearch;
 @property (strong, nonatomic) IBOutlet UIImageView *imgUpload;
 @property (strong, nonatomic) IBOutlet UIButton *btnImagePicker;
-
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
+@property (strong, nonatomic) CLLocationManager *locationManager;
+@property (strong, nonatomic) IBOutlet UITextField *txtLocation;
+@property (strong, nonatomic) IBOutlet UITextField *txtUsername;
+@property (strong, nonatomic) IBOutlet UITextView *txtComment;
+@property (strong, nonatomic) CLLocation *userLocation;
 @end
--- ResumoProva/NewCommentViewController.m
+++ ResumoProva/NewCommentViewController.m
@@ -7,9 +7,8 @@
 //
 
 #import "NewCommentViewController.h"
-#import <MapKit/MapKit.h>
-
-
+#import <AFNetworking.h>
+#import <SVProgressHUD.h>
 
 @interface NewCommentViewController ()
 
@@ -19,9 +18,63 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self initLocationService];
 }
 
+-(void)initLocationService{
+    self.locationManager = [[CLLocationManager alloc] init];
+    self.locationManager.delegate = self;
+    self.locationManager.distanceFilter = kCLDistanceFilterNone;
+    self.locationManager.desiredAccuracy = kCLLocationAccuracyBestForNavigation;
+    [self.locationManager startUpdatingHeading];
+    [self addGestureToMap];
+}
+
+
+
 - (IBAction)btnCadastrar:(id)sender {
+        [SVProgressHUD show];
+        AFHTTPRequestOperationManager	*manager	=	[AFHTTPRequestOperationManager manager];
+        
+        manager.requestSerializer = [AFJSONRequestSerializer serializer];
+        NSDictionary	*parameters	=	@{@"comment[user]":	self.txtUsername.text,
+                                          @"comment[content]": self.txtComment.text,
+                                          @"comment[lat]"  : [NSNumber numberWithDouble: self.userLocation.coordinate.latitude],
+                                          @"comment[lng]" :  [NSNumber numberWithDouble: self.userLocation.coordinate.longitude]  };
+        NSData *imageData = UIImageJPEGRepresentation(self.imgUpload.image,0.5);
+    
+        NSString * fileName = [self getCurrentDate];
+        [manager
+         POST:@"https://teste-aula-ios.herokuapp.com/comments.json"
+         parameters:parameters
+         constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+             [formData appendPartWithFileData:imageData name:@"comment[picture]" fileName:[NSString stringWithFormat:@"img_%@.jpg",fileName ] mimeType:@"image/jpeg" ];
+         }
+         
+         success:^(AFHTTPRequestOperation	*operation,	id responseObject)	{
+             NSLog(@"JSON:	%@",	responseObject);
+             [SVProgressHUD dismiss];
+             [ self.navigationController popViewControllerAnimated:YES];
+         }
+         failure:^(AFHTTPRequestOperation	*operation,	NSError	*error)	{
+             [SVProgressHUD dismiss];
+             NSInteger * returnStatus = [operation.response statusCode];
+             if(  returnStatus == 401 ) {
+                 [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+                 NSLog(@"Rolou hein esse upload maroto!");
+             }
+             NSLog(@"Error:	%@",	error);
+             NSLog(@"Error:	%@",	operation.responseString);
+             
+         }];
+}
+
+-(NSString * ) getCurrentDate{
+    NSDate *currentDate = [NSDate date];
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    [dateFormatter setDateFormat:@"ddMMYYHHmmss"];
+    NSString *dateString = [dateFormatter stringFromDate:currentDate];
+    return dateString;
 }
 
 - (IBAction)loadImage:(id)sender {
@@ -82,5 +135,99 @@
     [controller presentViewController: cameraUI animated: YES completion:nil];  return YES;
 }
 
+-(void)addGestureToMap{
+    UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(tapMap:)];
+    tapGesture.numberOfTapsRequired = 1;
+    tapGesture.numberOfTouchesRequired = 1;
+    tapGesture.delaysTouchesBegan = YES;
+    
+    [tapGesture setCancelsTouchesInView:YES];
+    [self.mapView addGestureRecognizer:tapGesture];
+}
 
+-(void)tapMap:(UITapGestureRecognizer *)recognizer{
+    CGPoint touchPoint = [recognizer locationInView:self.mapView];
+    CLLocationCoordinate2D touchMapCoordinate = [self.mapView convertPoint:touchPoint toCoordinateFromView:self.mapView];
+    CLLocation *location  = [[CLLocation alloc] initWithLatitude:touchMapCoordinate.latitude longitude:touchMapCoordinate.longitude];
+    [self markPoint:location];
+}
+
+-(void)markPoint: (CLLocation*) lugar{
+    MKPointAnnotation *point = [[MKPointAnnotation alloc] init];
+    CLLocationCoordinate2D coordinate = CLLocationCoordinate2DMake(lugar.coordinate.latitude, lugar.coordinate.longitude);
+    NSString* placeFound = [self getCity:lugar];
+    NSLog(@"Place atribuido é: %@", placeFound);
+    point.title = placeFound;
+    self.txtLocation.text = placeFound;
+    point.coordinate = coordinate;
+    [self.mapView addAnnotation:point];
+}
+
+-(NSString*)getCity: (CLLocation *) location {
+    CLGeocoder *geoCoder = [[CLGeocoder alloc] init];
+    __block CLPlacemark *placemark = nil;;
+    [geoCoder reverseGeocodeLocation:location completionHandler:^(NSArray<CLPlacemark *> * _Nullable placemarks, NSError * _Nullable error) {
+        placemark = placemarks[0];
+    }];
+    NSLog(@"Retorno é %@", placemark.locality);
+    return placemark.locality;
+}
+
+-(BOOL)textFieldShouldReturn:(UITextField *)textField{
+    [self startSearch:self.txtLocation.text];
+    return NO;
+}
+
+-(void)startSearch:(NSString *)searchString {
+    
+    [SVProgressHUD show];
+    if (self.localSearch.searching)
+    {
+        [self.localSearch cancel];
+    }
+    
+    MKLocalSearchRequest *request = [[MKLocalSearchRequest alloc] init];
+    request.naturalLanguageQuery = searchString;
+    
+    MKLocalSearchCompletionHandler completionHandler = ^(MKLocalSearchResponse *response, NSError *error) {
+        
+        if (error != nil) {
+            [self.localSearch cancel];
+            self.localSearch = nil;
+            NSLog(@"Erro");
+        } else {
+            if([response mapItems].count > 0){
+                NSArray* arrayLocations = [response mapItems];
+                MKMapItem* item = arrayLocations[0];
+                self.userLocation = item.placemark.location;
+                [self centerMap:item.placemark.location];
+                [self markPoint:item.placemark.location];
+                NSLog(@"%lu locations found ", (unsigned long)arrayLocations.count);
+            }else{
+                NSLog(@"No location found");
+            }
+        }
+        
+        [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    };
+    
+    if (self.localSearch != nil) {
+        self.localSearch = nil;
+    }
+    self.localSearch = [[MKLocalSearch alloc] initWithRequest:request];
+    
+    [self.localSearch startWithCompletionHandler:completionHandler];
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    [SVProgressHUD dismiss];
+}
+
+-(void)centerMap:(CLLocation *)location{
+    MKCoordinateRegion newRegion;
+    newRegion.center.latitude = location.coordinate.latitude;
+    newRegion.center.longitude = location.coordinate.longitude;
+    newRegion.span.latitudeDelta = 0.0005;
+    newRegion.span.longitudeDelta = 0.0005;
+    [self.mapView setRegion:newRegion];
+    
+}
 @end
